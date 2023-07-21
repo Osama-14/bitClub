b2d82045e11c42492e16fc027fbe67aac18305ed
Osama-14 <mohd.osama42000@gmail.com>
2023-07-21 12:16:02 +0500

    routes_done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, link, Link } from 'react-router-dom';
+import { layout, Typography, Space, Layout } from 'antd';
+import { Navbar, Exchanges, Homepage, CryptoDetails, Cryptocurrencies, News } from './components';
+import '../src/app.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route  path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route  path="/exchanges" element={<Exchanges />} />
+              <Route  path="/news" element={<News />} />
+              <Route  path="/crypto/:coinId" element={<CryptoDetails />} />
+
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            BitClub <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+
+          </Space>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
